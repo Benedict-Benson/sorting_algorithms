9900b855f88889c738bd0bcf20927c32c722d012
ben_ben <125489479+Benedict-Benson@users.noreply.github.com>
2023-09-24 17:01:45 +0300

    Add files via upload
    sorting

---
+++ 100-shell_sort.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order using Shell sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	/* Calculate the initial gap using Knuth sequence */
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap >= 1)
+	{
+		/* Perform insertion sort with the current gap */
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+
+			array[j] = temp;
+		}
+
+		/* Print the array for this gap value */
+		print_array(array, size);
+
+		/* Reduce the gap according to Knuth sequence */
+		gap /= 3;
+	}
+}
+
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap >= 1)
+	{
+		gap_sort(array, size, gap);
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
